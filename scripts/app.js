5d8cc5585c6e61c2b55260e038ee34db46ab17c8
grimmdude <grimmdude@gmail.com>
2010-09-04 00:27:04 -0700

    fixed prototype/jquery conflicts.  jtab is ready to roll now

--- scripts/app.js
+++ scripts/app.js
@@ -1,26 +1,29 @@
+//  jQuery no conflict mode for no prototype issues
+ jQuery.noConflict();
+
 // Setup events after dom is loaded
-$(document).ready(function() {
+jQuery(document).ready(function() {
 
 	//  jquery slider toggle for #instructions
-	$('#toggle_instr').click(function() {
-		$('#instructions').slideToggle('medium', function() { });
+	jQuery('#toggle_instr').click(function() {
+		jQuery('#instructions').slideToggle('medium', function() { });
 	});
 
 	//  apply .odd to every other table row for readability
-	$('#chord_selector tr:odd').addClass('odd');
+	jQuery('#chord_selector tr:odd').addClass('odd');
 
 	//  apply .highlight to each of the above td
-	$('#chord_selector td').addClass('highlight');
+	jQuery('#chord_selector td').addClass('highlight');
 
 	//  toggle when clicked to .selected and add a unique chord count id - ONLY ALLOW 5
-	$('#chord_selector td').click(function () {
-		$(this).toggleClass('selected');
+	jQuery('#chord_selector td').click(function () {
+		jQuery(this).toggleClass('selected');
 		namethatkey();		
 	});
 
 	//  click to remove .selected and from chord table (clears selections)
-	$('#clear_selected').click(function() {
-		$('#chord_selector td').removeClass('selected');
+	jQuery('#clear_selected').click(function() {
+		jQuery('#chord_selector td').removeClass('selected');
 		namethatkey();
 	});
 });
